121f86d4882a6d79b27e8d8b66c9a2cef05a1cff
JoakimTronstad <joakimtronstad@gmail.com>
2022-02-09 00:12:40 +0100

    edit PHP

--- php/dbh.inc.php
+++ php/dbh.inc.php
@@ -1,8 +1,8 @@
 <?php
 
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "swimmers";
+$dbServername = "db-mysql-ams3-37368-do-user-9992652-0.b.db.ondigitalocean.com";
+$dbUsername = "doadmin";
+$dbPassword = "qLQxuq2jfYaPwptP";
+$dbName = "defaultdb";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
