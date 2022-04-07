4a0e6f1e2ef680e0522821146102efc98ec1e769
JoakimTronstad <joakimtronstad@gmail.com>
2022-04-07 17:21:04 +0200

    edit files

--- app.py
+++ app.py
@@ -66,9 +66,9 @@ def user(swimmer_id):
 def index():
     return render_template("index.html")
 
-@app.route('/index.html')
-def index_():
-    return render_template("index.html")
+#@app.route('/index.html')
+#def index_():
+#    return render_template("index.html")
 
 @app.route('/point_time.html')
 def point_time():
