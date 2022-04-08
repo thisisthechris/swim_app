b90bb3667c2ca8b935feb62efdd045c07b28d3f3
Chris Hunt <mail@thisisthechris.co.uk>
2022-04-08 02:45:18 +0100

    Removes PHP code, adds template loop with swimmers and displays user data

--- templates/tid_for_svømmer.php
+++ templates/tid_for_svømmer.php
@@ -1,9 +1,6 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <?php
-      include_once 'php/dbh.inc.php';
-    ?>
     <meta charset="utf-8">
     <title>Tid for svømmer</title>
     <link rel="stylesheet" href="{{ url_for('static', filename='css/style.css') }}">
@@ -23,71 +20,53 @@
         </div>
       </div>
     </header>
-    <main style="padding-top: 150px;">
+    <main style="padding-top: 350px;">
       <script>
         function swimmerDisplay(result){
-          form.displaySwimmer.value=result;
+          // get the data from the database
+          fetch('/userdata/'+ result)
+            .then(response => response.json())
+            .then(data => 
+              {
+                console.log(data)
+
+                //display on page
+
+                document.getElementById("spanSwimmer").innerHTML = data.name;
+                document.getElementById("spanEvent").innerHTML = data.event;
+                document.getElementById("spanTime").innerHTML = data.time;
+                document.getElementById("spanPoints").innerHTML = data.points;
+                document.getElementById("spanDate").innerHTML = data.date;
+                document.getElementById("spanPool").innerHTML = data.pool;
+              }
+            );
         }
       </script>
       <div class="container">
 		    <form id="myForm" name="form">
-		    <div class="display">
-          <div class="row">
-            <input type="text" placeholder="Swimmer" name="displaySwimmer" id="swimmer" />
-          </div>
-        </div>
         <div class="dropdown">
           <button class="dropbtn">Swimmers</button>
           <div class="dropdown-content">
-            <input type="button" name="joakim" value="Joakim Tronstad" onClick="swimmerDisplay(joakim.value)">
-            <input type="button" name="kristian" value="Kristian Tronstad" onClick="swimmerDisplay(kristian.value)">
-            <input type="button" name="julius" value="Julius Aleksander Stähr Aarnes" onClick="swimmerDisplay(julius.value)">
-            <input type="button" name="oscarSI" value="Oscar Smeby Iden" onClick="swimmerDisplay(oscarSI.value)">
-            <input type="button" name="aksel" value="Aksel Smeby Iden" onClick="swimmerDisplay(aksel.value)">
-            <input type="button" name="ane" value="Ane Hanh Basberg" onClick="swimmerDisplay(ane.value)">
-            <input type="button" name="liv" value="Liv Hanh Basberg" onClick="swimmerDisplay(liv.value)">
-            <input type="button" name="ella" value="Ella Magnussen" onClick="swimmerDisplay(ella.value)">
-            <input type="button" name="jonas" value="Jonas Magnussen" onClick="swimmerDisplay(jonas.value)">
-            <input type="button" name="felix" value="Felix Richard Stavnes" onClick="swimmerDisplay(felix.value)">
-            <input type="button" name="fredrik" value="Fredrik Christopher Almnes" onClick="swimmerDisplay(fredrik.value)">
-            <input type="button" name="hannah" value="Hannah Ruud Raustøl" onClick="swimmerDisplay(hannah.value)">
-            <input type="button" name="oscarM" value="Oscar Lars Christian Megård" onClick="swimmerDisplay(oscarM.value)">
-            <input type="button" name="pauline" value="Pauline Hellesø Eriksen" onClick="swimmerDisplay(pauline.value)">
-            <input type="button" name="sofie" value="Sofie Helena Østbye" onClick="swimmerDisplay(sofie.value)">
-            <input type="button" name="william" value="William Torstveit Hansen" onClick="swimmerDisplay(william.value)">
-            <input type="button" name="zoe" value="Zoé Helen Sivertsen" onClick="swimmerDisplay(zoe.value)">
+          {%for swimmer in swimmers%}
+            <input type="button" name="{{swimmer.id}}" value="{{swimmer.name}}" onClick="swimmerDisplay({{swimmer.id}})">
+          {%endfor%}
           </div>
         </div>
+		    <div class="display">
+          <div class="row">
+            <div class="user">
+              <p>Swimmer: <span id="spanSwimmer"></span></p>
+              <p>Event: <span id="spanEvent"></span></p>
+              <p>Time: <span id="spanTime"></span></p>
+              <p>Points: <span id="spanPoints"></span></p>
+              <p>Date: <span id="spanDate"></span></p>
+              <p>Pool: <span id="spanPool"></span></p>
+            </div>
+          </div>
+        </div>
+        
         </form>
       </div>
-      <?php
-        $sql = "SELECT * FROM long_course_events;";
-        $result = mysqli_query($conn, $sql);
-        $resultCheck = mysqli_num_rows($result);
-
-        if ($resultCheck > 0) {
-        ?>
-        <table class='table table-bordered table-striped'>
-          <tr>
-            <td>time</td>
-          </tr>
-          <?php
-          while ($row = mysqli_fetch_assoc($result)) {
-            ?>
-            <tr>
-              <td><?php echo $row['time']; ?></td>
-            </tr>
-            <?php
-            $i++;
-          }
-          ?>
-          </table>
-          <?php
-        }
-        else{
-          echo "No results found"
-        }
-      ?>
     </main>
   </body>
 </html>
